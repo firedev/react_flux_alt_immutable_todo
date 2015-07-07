9fe0bd6a54966406516fb192b23959bee1b29640
Nick Ostrovsky <nick@firedev.com>
2015-07-07 22:54:53 +0700

    renamed element

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,4 +5,4 @@ require('bootstrap/less/bootstrap.less');
 import React from 'react/addons';
 import ReactImmutableAltTodoApp from 'components/ReactImmutableAltTodoApp.js';
 
-React.render(<ReactImmutableAltTodoApp />, document.getElementById('content'));
+React.render(<ReactImmutableAltTodoApp />, document.getElementById('todo-list'));
