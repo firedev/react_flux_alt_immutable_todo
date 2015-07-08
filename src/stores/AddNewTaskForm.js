2df18b9dbb0e4a456d26f55b2a3148dd688d2db7
Nick Ostrovsky <nick@firedev.com>
2015-07-08 10:33:54 +0700

    Stores/AddNewTaskForm

---
+++ src/stores/AddNewTaskForm.js
@@ -0,0 +1,34 @@
+import Alt from 'lib/Alt';
+import Actions from 'actions/AddNewTaskForm';
+
+class AddNewTaskFormStore {
+  constructor() {
+    this.validationError = '';
+    this.content = '';
+    this.submittable = false;
+
+    let { changeContent, clearForm } = Actions;
+    this.bindListeners({ changeContent, clearForm });
+  }
+
+  changeContent(newContent) {
+    let validationError = this.validate(newContent),
+    submittable = validationError.length === 0;
+
+    this.setState({ validationError,
+      content: newContent,
+      submittable });
+  }
+
+  clearForm() {
+    this.setState({ validationError: '',
+      content: '',
+      submittable: false });
+  }
+
+  validate(newContent) {
+    return (newContent.length > 3) ? '' : 'Task content should be longer';
+  }
+}
+
+export default Alt.createStore(AddNewTaskFormStore);
